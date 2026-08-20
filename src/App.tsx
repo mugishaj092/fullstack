ddbc0e60ca13996c5c49f5eb6420b4b1e14acaad
mugishaj092 <mugishajoseph092@gmail.com>
2026-08-20 17:50:42 +0200

    will add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
-          <p>
+          <p className='text-red-200'>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
